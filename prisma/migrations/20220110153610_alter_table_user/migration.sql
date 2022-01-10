df436f79facf1d5f79d5b9988022ccd39c02e33c
Gustavo Evaristo <gug.henri1@gmail.com>
2022-01-10 14:19:08 -0300

    migration

--- prisma/migrations/20220110153610_alter_table_user/migration.sql
+++ prisma/migrations/20220110153610_alter_table_user/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE "User";
 
 -- CreateTable
 CREATE TABLE "users" (
---
+++ prisma/migrations/20220110171821_create_refresh_token/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "refresh-token" (
+    "id" TEXT NOT NULL,
+    "expiresIn" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "refresh-token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh-token_userId_key" ON "refresh-token"("userId");
+
+-- AddForeignKey
+ALTER TABLE "refresh-token" ADD CONSTRAINT "refresh-token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
